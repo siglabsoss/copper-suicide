24b0ead1f8467e954ee5df0586634d0bc9df0493
David Kronstein <dkronstein@gmail.com>
2016-06-28 13:50:58 -0700

    Manually annotated FPGAs

---
+++ hardware/ReplicateFPGA.bat
@@ -0,0 +1,32 @@
+copy fpga00-edge.SchDoc fpga01-edge.SchDoc
+copy fpga00-edge.SchDoc fpga02-edge.SchDoc
+copy fpga00-edge.SchDoc fpga03-edge.SchDoc
+copy fpga00-edge.SchDoc fpga30-edge.SchDoc
+copy fpga00-edge.SchDoc fpga31-edge.SchDoc
+copy fpga00-edge.SchDoc fpga32-edge.SchDoc
+copy fpga00-edge.SchDoc fpga33-edge.SchDoc
+
+copy fpga10-center.SchDoc fpga11-center.SchDoc
+copy fpga10-center.SchDoc fpga12-center.SchDoc
+copy fpga10-center.SchDoc fpga13-center.SchDoc
+copy fpga10-center.SchDoc fpga20-center.SchDoc
+copy fpga10-center.SchDoc fpga21-center.SchDoc
+copy fpga10-center.SchDoc fpga22-center.SchDoc
+copy fpga10-center.SchDoc fpga23-center.SchDoc
+
+copy fpga00-serdes-power.SchDoc fpga01-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga02-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga03-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga10-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga11-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga12-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga13-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga20-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga21-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga22-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga23-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga30-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga31-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga32-serdes-power.SchDoc
+copy fpga00-serdes-power.SchDoc fpga33-serdes-power.SchDoc
+
